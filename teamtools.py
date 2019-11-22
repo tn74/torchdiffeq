773c7368a97e016170a0e5e6f4c85e6c24a65bd5
Trishul Nagenalli <tn74@duke.edu>
2019-11-22 12:59:10 -0500

    Team tools method trains spiral

--- teamtools.py
+++ teamtools.py
@@ -11,23 +11,16 @@ import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 import math
 
-from torchdiffeq.torchdiffeq import odeint_adjoint as odeint
-# from torchdiffeq.torchdiffeq import odeint
+from torchdiffeq import odeint_adjoint
+from torchdiffeq import odeint
 
 
-def train(model, train_loader, ode_propogator,
-          data_size = 1000,
-          batch_time = 10,
-          batch_size = 20,
+def train(model, train_loader, ode_propogator, optimizer,
           niters = 1000,
           test_freq = 20,
-          viz = True,
           gpu = 0,
           adjoint = 0):
-    optimizer = optim.Adam(func.parameters(), lr=1e-3)
-    end = time.time()
     ii = 0
-
     for itr in range(1, args.niters + 1):
         optimizer.zero_grad()
         batch_y0, batch_t, batch_y = train_loader()
@@ -38,8 +31,68 @@ def train(model, train_loader, ode_propogator,
 
         if itr % args.test_freq == 0:
             with torch.no_grad():
-                pred_y = odeint(func, true_y0, t)
+                pred_y = odeint(model, true_y0, t)
                 loss = torch.mean(torch.abs(pred_y - true_y))
                 print('Iter {:04d} | Total Loss {:.6f}'.format(itr, loss.item()))
-                visualize(true_y, pred_y, func, ii)
                 ii += 1
+
+if __name__ == "__main__":
+    class Config:
+      data_size = 1000
+      batch_time = 10
+      batch_size = 20
+      niters = 1000
+      test_freq = 20
+      viz = True
+      gpu = 0
+      adjoint = 0
+    args = Config()
+
+    class Spiral(nn.Module):
+      angle = 45
+      # transform_matrix = 0.9 * torch.Tensor([
+      #     [math.cos(angle * math.pi/180),   -math.sin(angle * math.pi/180)],
+      #     [math.cos(angle * math.pi/180),    math.cos(angle * math.pi/180)]
+      # ])
+      transform_matrix = torch.tensor([[-0.1, 2.0], [-2.0, -0.1]])
+
+      def forward(self, t, y):
+        return torch.mm(y, self.transform_matrix)
+
+    t = torch.linspace(0., 5., args.data_size)
+    true_y0 = torch.tensor([[2., 0.]])
+    with torch.no_grad():
+        true_y = odeint(Spiral(), true_y0, t, method='dopri5')
+
+    def get_batch():
+        s = torch.from_numpy(np.random.choice(np.arange(args.data_size - args.batch_time, dtype=np.int64), args.batch_size, replace=False))
+        batch_y0 = true_y[s]  # (M, D)
+        batch_t = t[:args.batch_time]  # (T)
+        batch_y = torch.stack([true_y[s + i] for i in range(args.batch_time)], dim=0)  # (T, M, D)
+        return batch_y0, batch_t, batch_y
+
+    class ODEFunc(nn.Module):
+        def __init__(self):
+            super(ODEFunc, self).__init__()
+            self.net = nn.Sequential(
+                nn.Linear(2, 50),
+                nn.Tanh(),
+                nn.Linear(50, 2),
+                # nn.Linear(2, 2)
+            )
+
+            for m in self.net.modules():
+                if isinstance(m, nn.Linear):
+                    nn.init.normal_(m.weight, mean=0, std=0.1)
+                    nn.init.constant_(m.bias, val=0)
+
+        def forward(self, t, y):
+            return self.net(y)
+    model = ODEFunc()
+    train_loader = get_batch
+    optimizer = optim.Adam(model.parameters(), lr=1e-3)
+    train(model, train_loader, odeint, optimizer)
+
+
+
+
